7dce73b6b63bfe0beb6bd7a94cd422f65dae8fd6
ORI <gl33556@gmail.com>
2024-11-13 12:29:48 +0200

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 const shareModal = document.querySelector("#modal");
 const shareButton = document.querySelector("#share-icon");
+const shareIcon = document.querySelector(".card__share-icon");
+const mediaQuery = window.matchMedia("(max-width: 376px)");
 
 shareButton.addEventListener("click", () => {
   shareModal.classList.toggle("modal_opened");
-  //   shareButton.style.background = "hsl(214, 17%, 51%)";
+  if (mediaQuery.matches && shareModal.classList.contains("modal_opened")) {
+    shareButton.style.background = "hsl(217, 19%, 35%)";
+  } else {
+    shareButton.style.background = "hsl(210, 46%, 95%)";
+  }
 });
 
 // Close the modal when clicking anywhere outside the modal content
@@ -12,7 +18,6 @@ document.addEventListener("click", handleOverlay);
 function handleOverlay(evt) {
   // Check if modal is open
   if (shareModal.classList.contains("modal_opened")) {
-    // const isClickOnButton = shareButton.contains(evt.target);
     const isClickInsideModal =
       shareModal.contains(evt.target) || shareButton.contains(evt.target);
 
